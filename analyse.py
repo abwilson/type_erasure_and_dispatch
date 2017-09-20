0a5e73a28f3f118ca604cbb383e2244f0ec69024
Ab Wilson <norman.wilson@gmail.com>
2017-09-21 00:09:46 +0100

    Analyse the data

---
+++ analyse.py
@@ -0,0 +1,34 @@
+import csv
+import sys
+import numpy
+
+files = sys.argv[1:]
+
+data = {}
+
+for f in files:
+    with open(f) as file:
+        name = f.split(".")[0]
+        d = {
+            "times": [],
+            "cycles": []
+            }
+        data[name] = d
+        times = d["times"]
+        cycles = d["cycles"]
+
+        lines = csv.reader(file)
+        next(lines)
+
+        for line in lines:
+            # print line
+            bench, msgs, t, _1, c, _2, _3 = line
+            times.append(float(t))
+            cycles.append(int(c))
+
+        msgs = int(msgs)
+
+        def perMsg(v):
+            return (v / msgs) * 1000000
+
+        print "%s %fms %fms %d %d" % (name, perMsg(numpy.mean(times)), perMsg(numpy.std(times)), numpy.mean(cycles) / msgs, numpy.std(cycles) / msgs)
